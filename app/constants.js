d77e522cc8c1b92dbd7b03dddfb459031590c2d8
shamlikhan <shamlikhan@qburst.com>
2020-04-01 16:44:14 +0530

    User Authentication
    POST /register API, user validation

---
+++ app/constants.js
@@ -0,0 +1,19 @@
+const Fields = {
+    _ID: '_id',
+    EMAIL: 'email',
+    ID: 'id',
+    NAME: 'name',
+    PASSWORD: 'password',
+    STATUS: 'status',
+    UPDATED_AT: 'updated_at'
+};
+
+const Status = {
+    ACTIVE: 'active',
+    INACTIVE: 'inactive'
+};
+
+module.exports = {
+    Fields,
+    Status
+};
