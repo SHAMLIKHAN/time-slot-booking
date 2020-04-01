243a768bc5b90bda18bc97deb758791f318318e4
shamlikhan <shamlikhan@qburst.com>
2020-04-01 15:48:44 +0530

    Basic Structure
    Database connection established

--- config.js
+++ config.js
@@ -1,5 +1,16 @@
+const host = process.env.DEVELOPMENT_MONGODB_HOST;
+const user = process.env.DEVELOPMENT_MONGODB_USERNAME;
+const pass = process.env.DEVELOPMENT_MONGODB_PASSWORD;
+const port = process.env.DEVELOPMENT_MONGODB_PORT;
+const auth = process.env.DEVELOPMENT_MONGODB_AUTH_SOURCE;
+const db = process.env.DEVELOPMENT_MONGODB_DATABASE;
+
 const app = process.env.PORT || 8080;
+const mongoURI = `mongodb://${host}:${port}/${db}`;
+const mongoDB = db;
 
 module.exports = {
-  PORT: app
+	MONGO_URL: mongoURI,
+	MONGO_DB: mongoDB,
+	PORT: app
 };
---
+++ services/db_conn.js
@@ -0,0 +1,17 @@
+const mongoClient = require('mongodb').MongoClient;
+const config = require('../config');
+
+async function setupMongoDB() {
+    try {
+        const client = await mongoClient.connect(config.MONGO_URL, { useUnifiedTopology: true });
+        const database = config.MONGO_DB;
+        return await client.db(database);
+    } catch (err) {
+        console.log("Error connecting database!", err);
+        return err;
+    }
+}
+
+module.exports = {
+    setupDatabase: setupMongoDB
+};
